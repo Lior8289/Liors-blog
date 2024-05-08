104ddc692a34ee17ee28debcc58ff779a13678a7
Lior Morali <lior8289@gmail.com>
2024-05-09 02:25:44 +0300

    create a post api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import postRoutes from "./routes/post.route.js";
+
 import { stat } from "fs";
 import cookieParser from "cookie-parser";
 dotenv.config();
@@ -26,6 +28,7 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/post", postRoutes);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
