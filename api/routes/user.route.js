9f63635fa9ae4b40d58213deac619d1d7652f697
Lior Morali <lior8289@gmail.com>
2024-05-10 10:02:43 +0300

    create getusers api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -4,6 +4,7 @@ import {
   updateUser,
   deleteUser,
   signout,
+  getusers,
 } from "../controllers/user.controller.js";
 import {} from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
@@ -14,4 +15,5 @@ router.get("/test", test);
 router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
 router.post("/signout", signout);
+router.get("/getusers", verifyToken, getusers);
 export default router;
