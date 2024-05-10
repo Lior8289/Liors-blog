a0b0d49a2475a49300ab296da36c679638e31b29
Lior Morali <lior8289@gmail.com>
2024-05-10 23:37:51 +0300

    add comment section to the post page

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import { Spinner, Button } from "flowbite-react";
 import CallToAction from "../components/CallToAction";
+import CommentSection from "../components/CommentSection";
 
 export default function PostPage() {
   const { postSlug } = useParams();
@@ -39,7 +40,7 @@ export default function PostPage() {
         <Spinner size="xl"></Spinner>
       </div>
     );
-  } else {
+  } else if (post) {
     return (
       <main className="p-3 flex flex-col max-w-6xl mx-auto min-h-screen">
         <h1 className="text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl">
@@ -67,12 +68,13 @@ export default function PostPage() {
           </span>
         </div>
         <div
-          className="p-3 max-w-2xl mx-auto w-full post-content"
+          className="p-3 max-w-2xl mx-auto w-full post-content text-black"
           dangerouslySetInnerHTML={{ __html: post && post.content }}
         ></div>
         <div className="max-w-4xl mx-auto w-full">
           <CallToAction />
         </div>
+        <CommentSection postId={post._id} />
       </main>
     );
   }
