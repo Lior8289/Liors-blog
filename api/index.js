6e8d3da25ca24e518601e06a5401ba339f817c22
Lior Morali <lior8289@gmail.com>
2024-05-14 17:18:36 +0300

    few fixes

--- api/index.js
+++ api/index.js
@@ -4,25 +4,25 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import postRoutes from "./routes/post.route.js";
-import commentRoute from "./routes/comment.route.js";
+import commentRoutes from "./routes/comment.route.js";
+import cookieParser from "cookie-parser";
 import path from "path";
 
-import { stat } from "fs";
-import cookieParser from "cookie-parser";
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
-    console.log("MongoDB is connected");
+    console.log("MongoDb is connected");
   })
-  .catch((error) => {
-    console.log(error);
+  .catch((err) => {
+    console.log(err);
   });
 
 const __dirname = path.resolve();
 
 const app = express();
+
 app.use(express.json());
 app.use(cookieParser());
 
@@ -33,11 +33,11 @@ app.listen(3000, () => {
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
-app.use("/api/comment", commentRoute);
+app.use("/api/comment", commentRoutes);
 
 app.use(express.static(path.join(__dirname, "/client/dist")));
 
-app.get("*", (res, req) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
