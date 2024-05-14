e319571afef0cb0abbef4dd7a98a040e63732682
Lior Morali <lior8289@gmail.com>
2024-05-14 16:45:47 +0300

    complete about and projects pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,46 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div className="">
+          <h1 className="text-3xl font-semibold text-center my-7">
+            About Lior's Blog
+          </h1>
+          <div className=" text-md dark:text-gray-300 text-gray-500 flex flex-col gap-6">
+            <p>
+              Welcome to Lior's Blog, my first FullStack Project using MERN!
+              Here, I invite you to delve into a world where I share my
+              thoughts, experiences, and passions through posts, articles, and
+              much more.
+            </p>
+            <p>
+              As the creator of this platform, I'm excited to offer you a
+              glimpse into my journey, both personal and professional. Through
+              Lior's Blog, I aim to create a space where we can engage in
+              meaningful conversations about a wide range of topics, from
+              technology and innovation to art, culture, and everything in
+              between.
+            </p>
+            <p>
+              This blog isn't just about sharing information—it's about building
+              a community. I hope to connect with like-minded individuals who
+              share a curiosity for learning and a passion for exploring new
+              ideas. Whether you're here to discover insights, gain inspiration,
+              or simply unwind with some captivating reads, I invite you to make
+              yourself at home and enjoy the content I've curated for you.
+            </p>
+            <p>
+              Join me on this adventure as we embark on a journey of discovery,
+              growth, and shared experiences. Together, let's explore the
+              boundless possibilities that the world of blogging has to offer.
+              Thank you for being a part of this journey, and I look forward to
+              connecting with you through Lior's Blog!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
