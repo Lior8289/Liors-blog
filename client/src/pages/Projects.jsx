b6a726be82d9c22d2a06b145e1bfae8944233ce6
Lior Morali <lior8289@gmail.com>
2024-05-15 12:44:28 +0300

    Revert "complete about and projects pages"
    This reverts commit e319571afef0cb0abbef4dd7a98a040e63732682.

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,23 +1,5 @@
 import React from "react";
-import CallToAction from "../components/CallToAction";
-export default function Projects() {
-  return (
-    <div className="min-h-screen mx-auto justify-center items-center gap-6 p-3 ">
-      <div className="">
-        <h1 className="text-3xl text-center p-10 ">Projects</h1>
-        <p className="text-xl text-center p-1">
-          Here there's some of my projects
-        </p>
-      </div>
 
-      <img
-        className="mx-auto flex flex-wrap mt-20 mb-20"
-        src="https://t4.ftcdn.net/jpg/00/89/02/67/360_F_89026793_eyw5a7WCQE0y1RHsizu41uhj7YStgvAA.jpg"
-        alt="under construction"
-      />
-      <div className="">
-        <CallToAction />
-      </div>
-    </div>
-  );
+export default function Projects() {
+  return <div>Projects</div>;
 }
